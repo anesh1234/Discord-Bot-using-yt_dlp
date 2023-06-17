c900793542ef3ac633ad28f2e5e44d61123bce25
anesh1234 <112165922+anesh1234@users.noreply.github.com>
2023-06-17 11:34:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
 from __future__ import unicode_literals
 import os
 import discord
+import yt_dlp
 from yt_dlp import YoutubeDL
 from dotenv import load_dotenv
 
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client(intents=discord.Intents.all())
 tree = discord.app_commands.CommandTree(client)
 
-@tree.command(name = "respond", description = "my first application command", guild=discord.Object(id=223167064112627714)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_command(interaction):
-    await interaction.response.send_message("Hei, jeg lever")
 
 @tree.command(name = "download_yt_audio", description = "Give me a URL to extract audio from", guild=discord.Object(id=223167064112627714))
 async def seccond_command(interaction: discord.Interaction, text: str):
     await interaction.response.defer()
+    
     brukerid = interaction.user.id
     mp3_filepath = fr'C:\Users\ander\Documents\Progging\DiscordBotPython\{brukerid}.mp3'
     ydl_opts = {
@@ -33,7 +30,12 @@ async def seccond_command(interaction: discord.Interaction, text: str):
     }
     
     with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([f'{text}'])
+        try:
+            ydl.download([f'{text}'])
+        except yt_dlp.utils.DownloadError:
+            await interaction.followup.send("Feil: Er dette en fullstendig og gyldig URL?")
+            return
+        
     
     mp3_file = discord.File(fp=mp3_filepath, filename='Funny meme.mp3')
     channel = await interaction.user.create_dm()
@@ -42,6 +44,7 @@ async def seccond_command(interaction: discord.Interaction, text: str):
     await interaction.followup.send("Sjekk postkassen din. Mvh")
     
     os.remove(mp3_filepath) 
+
 
 @client.event
 async def on_ready():
